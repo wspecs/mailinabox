911e7e8ba3d90987b7423da323cf7138dd732b9b
Wendly Saintil <wspecs@hotmail.com>
2020-02-08 19:16:34 -0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ if [ ! -d $HOME/mailinabox ]; then
 	echo Downloading Mail-in-a-Box $TAG. . .
 	git clone \
 		-b $TAG --depth 1 \
-		https://github.com/mail-in-a-box/mailinabox \
+		https://github.com/wspecs/mailinabox \
 		$HOME/mailinabox \
 		< /dev/null 2> /dev/null
 
